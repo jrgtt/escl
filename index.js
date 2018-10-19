efe70f537ae9be01cc48bdc9d1777963de4fa057
Jean Rigotti <gmgotti@protonmail.com>
2018-10-19 20:17:21 +0200

    This worked and I can't believe

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const vm = require('vm');
+
 const elasticsearch = require('elasticsearch');
+const repl = require('repl');
+
 const client = new elasticsearch.Client({
     host: 'localhost:9200',
     log: 'trace'
+});
+
+const r = repl.start();
+
+r.context = vm.createContext({
+    ...r.context,
+    escli: client,
+    elasticsearch,
 });

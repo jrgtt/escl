e2fbcf4911e4acdf4146e66b59f206e773620720
Jean Rigotti <jean@studocu.com>
2018-10-24 15:26:43 +0200

    Hardcoded api sent along with repl

--- repl.js
+++ repl.js
@@ -2,10 +2,12 @@ const vm = require('vm');
 
 const client = require('./client.js')();
 const repl = require('repl');
+const apis = require('./node_modules/elasticsearch/src/lib/apis/6_3.js');
 
 const r = repl.start();
 
 r.context = vm.createContext({
     ...r.context,
     escli: client,
+    apis
 });

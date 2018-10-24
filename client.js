3d05127e8d40e71a0be60f2d023ec422594bbc06
Jean Rigotti <jean@studocu.com>
2018-10-24 14:25:25 +0200

    Use configuration files for escli

--- client.js
+++ client.js
@@ -1,7 +1,18 @@
 const elasticsearch = require('elasticsearch');
+const configuration = require('cosmiconfig')('escli');
 
-module.exports = () => new elasticsearch.Client({
-    host: 'localhost:9200',
+let defaultClientConfig = {
+    host: 'http://localhost:9200',
     log: ['error', 'trace'],
-    apiVersion: '6.3',
-});
+    apiVersion: '6.3'
+};
+
+// search for `escli` in configuration files
+const customClientOptions = configuration.searchSync() || { config: {} };
+
+let config = {
+    ...defaultClientConfig,
+    ...customClientOptions.config,
+};
+
+module.exports = () => new elasticsearch.Client(config);

ab3adf6afb2c95e72308376a64ef62a0eb5e5ca8
Jean Rigotti <jean@studocu.com>
2018-10-30 11:09:32 +0100

    Expect a client option in configuration file

--- client.js
+++ client.js
@@ -1,18 +1,21 @@
-const elasticsearch = require('elasticsearch');
+const { Client } = require('elasticsearch');
 const configuration = require('cosmiconfig')('escli');
 
-let defaultClientConfig = {
-    host: 'http://localhost:9200',
-    log: ['error', 'trace'],
-    apiVersion: '6.3'
-};
+/**
+ * The aim of this project is not to help you build an elasticsearch
+ * client connection.  Hence I assume that the information and custom
+ * configuration should be provided by the user.
+ *
+ * To pass those configuration create a `.esclirc` (supported in
+ * different extensions such as `js`, `yaml`, `json`) file within your
+ * project or add a key named `escli` to your package.json.
+ *
+ * In the configuration add a `client` key where it will be passed
+ * directly to instantiate a Elasticsearch.Client.
+ *
+ * In doubt check the documentation:
+ * https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
+ **/
+const escliConfig  = configuration.searchSync() || { config: { client: {} } };
 
-// search for `escli` in configuration files
-const customClientOptions = configuration.searchSync() || { config: {} };
-
-let config = {
-    ...defaultClientConfig,
-    ...customClientOptions.config,
-};
-
-module.exports = () => new elasticsearch.Client(config);
+module.exports = () => new Client(escliConfig.config.client);

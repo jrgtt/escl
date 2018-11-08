d6f91d0afc820dfe5147ea073b21730d511236ab
Jean Rigotti <jean@studocu.com>
2018-11-08 14:34:39 +0100

    Rewrote the whole thing with yargs, more powerful than commander

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-const client = require('./client.js')();
+const yargs = require('yargs');
 const editor = require('./editor.js');
 
 const { validFilePath, validListOrString } = require('./utils/validation.js');
 
-program
-    .version('0.0.1', '-v --version')
-    .option(
-        '-e, --editor',
-        'Use your editor to write the parameters of the request'
-    )
-    .option(
-        '-f, --file <path>',
-        'JS or JSON file to be used as body request',
-        validFilePath
-    )
-    .option(
-        '--index <index>',
-        'Index or list of indices to operate on',
-        validListOrString
-    )
-    .option(
-        '--body <path>',
-        'File to be used as body parameter',
-        validFilePath
-    )
-    .option(
-        '--q <query>',
-        'Query in the Lucene query string syntax '
-    )
-    .arguments('<namespaceOrCmd> [cmd]')
-    .action((namespaceOrCmd, cmd) => {
-        // apply namespace if provided
-        const fn = cmd ? client[namespaceOrCmd][cmd] : client[namespaceOrCmd];
+const argv = yargs
+      .command('$0', 'Refer to elasticsearch documentation to check which methods you can use')
+      .option('editor', {
+          alias: 'e',
+          describe: 'Use your default editor or provide one to be used',
+          type: 'boolean'
+      })
+      .option('file', {
+          alias: 'f',
+          describe: 'JS/JSON file to be sent as parameters',
+          type: 'string',
+          coerce: validFilePath
+      })
+      .option('body', {
+          alias: 'b',
+          describe: 'JS/JSON file to be sent as body key in parameters',
+          type: 'string',
+          coerce: validFilePath
+      })
+      .help()
+      .argv;
 
-        if (program.editor) {
-            editor(program.file || null).then((res) => {
-                fn.call(client, res);
-            });
-        } else {
-            let res = program.file ? require(program.file) : {};
-            res = {
-                ...res,
-                ...program.index && { index: program.index },
-                ...program.q && { q: program.q },
-                ...program.body && { body: require(program.body) },
-            };
-            fn.call(client, res);
-        }
-    })
-    .parse(process.argv);
+const { _: [namespaceOrCmd, cmd] } = argv;
+const client = require('./client.js')();
+
+// TODO: throw some error if namespace not found
+const fn = cmd ? client[namespaceOrCmd][cmd] : client[namespaceOrCmd];
+
+if (argv.editor) {
+    editor(argv.file || null).then((res) => {
+        fn.call(client, res);
+    });
+} else {
+    let res = argv.file ? require(argv.file) : {};
+    res = {
+        ...res,
+        ...argv.index && { index: argv.index },
+        ...argv.q && { q: argv.q },
+        ...argv.body && { body: require(argv.body) },
+    };
+    fn.call(client, res);
+}

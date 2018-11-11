9d82ff0eb8bc43982c4317219e2e9be21e296f4d
Jean Rigotti <gmgotti@protonmail.com>
2018-11-11 22:54:11 +0100

    Move running logic of the into an specific file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs');
-const editor = require('./editor.js');
 
 const { validFilepath, validJSON } = require('./utils/validation.js');
 
@@ -37,32 +36,5 @@ const {
     ...options
 } = argv;
 
-// remove these options from the params
-delete options.e;
-delete options.editor;
-delete options.b;
-delete options.body;
-delete options.f;
-delete options.file;
-
-// the elasticsearch client
-const client = require('./client.js')();
-
-// TODO: throw some error if namespace not found
-const fn = cmd ? client[namespaceOrCmd][cmd] : client[namespaceOrCmd];
-
-if (argv.editor) {
-    editor(argv.file || null, options).then((res) => {
-        fn.call(client, res);
-    });
-} else {
-    // use file as full parameter (if provided)
-    let res = argv.file ? require(argv.file) : {};
-
-    res = {
-        ...res,
-        ...argv.body && { body: require(argv.body) },
-        ...options
-    };
-    fn.call(client, res);
-}
+require('./lib/steker.js')([namespaceOrCmd, cmd], options)
+    .catch(() => {}); // avoids closing the program in unhandled exception

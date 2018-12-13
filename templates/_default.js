ee3d5e1ecbabf09da4ff18ef700bdcd677b95f0d
Jean Rigotti <jean@studocu.com>
2018-12-13 13:59:32 +0100

    Template supports now json strings passed as option

--- templates/_default.js
+++ templates/_default.js
@@ -4,21 +4,34 @@ const builder = recast.types.builders;
 // initial code
 let code = 'module.exports = {};';
 
+const buildObject = (obj) => {
+    const properties = Object.keys(obj).map((k) => {
+        let val = obj[k];
+
+        if (typeof val === 'object') {
+            val = buildObject(val);
+        } else {
+            val = builder.literal(val);
+        }
+
+        return builder.objectProperty(
+            builder.identifier(k),
+            val
+        );
+    });
+
+    return builder.objectExpression(properties);
+};
+
 module.exports = (inheritOptions = {}) => {
     // the abstract syntax tree
     const ast = recast.parse(code);
 
     // add cli options to object tree
-    Object.keys(inheritOptions).forEach((k) => {
-        const val = inheritOptions[k];
+    const result = buildObject(inheritOptions);
 
-        ast.program.body[0].expression.right.properties.push(
-            builder.objectProperty(
-                builder.identifier(k),
-                builder.literal(val)
-            )
-        );
-    });
+    // put newly created object as right side of the ast
+    ast.program.body[0].expression.right = result;
 
     return recast.prettyPrint(ast).code;
 };
